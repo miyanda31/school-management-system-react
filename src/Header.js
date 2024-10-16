20078d5a5309ff9014a76c5390e8384255977f52
Augustine Mlowoka <chirwasekelerani@gmail.com>
2024-10-16 10:46:57 +0900

    Added Links

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function Header() {
+
+  var times = 20;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I understand
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <h1>Hello React {times} Lessons</h1>
       </div>
     );
   }
